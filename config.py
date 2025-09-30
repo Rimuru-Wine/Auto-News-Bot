b301d33f5819ba074e85341f313585891f4171fa
𝘙𝘪𝘮𝘶𝘳𝘶  [ 𝘞𝘪𝘯𝘦 🍷 ] <rimuruwine@gmail.com>
2025-09-30 22:16:53 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+API_ID = 123456
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token"
+MONGO_URI = "mongodb://localhost:27017/"
+ADMINS = [123456789]
+START_PIC = None  # Optional URL or local file path
