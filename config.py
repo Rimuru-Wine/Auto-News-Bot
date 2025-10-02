62de5c411ffc96a2c9e264978875c36272a0dae9
𝘙𝘪𝘮𝘶𝘳𝘶  [ 𝘞𝘪𝘯𝘦 🍷 ] <rimuruwine@gmail.com>
2025-10-03 02:18:16 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-API_ID = 123456
-API_HASH = "your_api_hash"
-BOT_TOKEN = "your_bot_token"
-MONGO_URI = "mongodb://localhost:27017/"
-ADMINS = [123456789]
+API_ID = 
+API_HASH = ""
+BOT_TOKEN = ""
+MONGO_URI = ""
+ADMINS = []
 START_PIC = None  # Optional URL or local file path
